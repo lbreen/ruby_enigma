85e1b77b01dd67690b4d3f2e5641d103d32b1b3f
Luke Breen <l.breen1@outlook.com>
2017-08-30 14:28:50 +0800

    Tests specify the rotor number and the Rotor model retrieves the key settings

--- spec/rotor_spec.rb
+++ spec/rotor_spec.rb
@@ -2,40 +2,54 @@ require 'rotor'
 require 'json'
 
 describe Rotor do
-  # Retrieve inital key settings
-  key_settings = JSON.parse(File.read('lib/key_settings.json'))
 
-  it "should be able to specify the start letter" do
-    start_letter = 'C'
-    rotor = Rotor.new(settings: key_settings['rotor_i'], start_letter: start_letter)
-    expect(rotor.start_letter).to eq("C")
-  end
-
-  it "should default to 'A' if no start letter is provided" do
-    rotor = Rotor.new(key_settings['rotor_i'])
-    expect(rotor.start_letter).to eq("A")
-  end
+  alphabet = ('A'..'Z').to_a
 
   it "should be able to specify which rotor to use" do
-    rotor_one = Rotor.new(key_settings['rotor_i'])
-    rotor_two = Rotor.new(key_settings['rotor_ii'])
+    rotor_one = Rotor.new(rotor_number: 'i')
+    rotor_two = Rotor.new(rotor_number: 'ii')
 
     expect(rotor_one.rotor_number).to eq("I")
     expect(rotor_two.rotor_number).to eq("II")
   end
 
-  it "should rotate with each letter entered" do
-    rotor = Rotor.new(settings: key_settings['rotor_i'])
-
-    expect(rotor.first_letter).to eq("A")
-    # Substitue
-    expect(rotor.first_letter).to eq("B")
+  it "should be able to specify the start letter" do
+    start_letter = alphabet.sample
+    rotor = Rotor.new(rotor_number: "i", start_letter: start_letter)
+    expect(rotor.start_letter).to eq(start_letter)
   end
 
-  it "should return the same output letter for the same input letter" do
-    rotor = Rotor.new(settings: key_settings['rotor_i'])
+  it "should default to 'A' if no start letter is provided" do
+    rotor = Rotor.new(rotor_number: 'i')
+    expect(rotor.start_letter).to eq("A")
+  end
 
-    rotor.substi
+  it "should rotate with each letter entered" do
+    start_letter = alphabet.sample
+    next_letter = (start_letter.ord + 1).chr
+    rotor = Rotor.new(rotor_number: 'i', start_letter: start_letter)
 
+    expect(rotor.first_letter).to eq(start_letter)
+    rotor.rotate!
+    expect(rotor.first_letter).to eq(next_letter)
+  end
+
+  it "should substitute the letter based on its index" do
+
+  end
+
+  it "should rotate the next rotor, after the first rotor has completed one turn" do
+    start_letter = alphabet.sample
+    next_letter = (letter.ord + 1).chr
+    rotor_one = Rotor.new(rotor_number: 'i', start_letter: start_letter)
+    rotor_two = Rotor.new(rotor_number: 'ii', start_letter: start_letter)
+
+    expect(rotor_two.first_letter).to eq(start_letter)
+
+    27.times do
+      rotor_one.rotate!
+    end
+
+    expect(rotor_two.first_letter).to eq(next_letter)
   end
 end
